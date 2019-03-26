83eb70fffc1efaf39efb4421feb519d77d160a8a
David Strootman <David.strootman@gmail.com>
2019-03-26 13:25:29 +0100

    Update linerider speed

--- src/Jhin.cpp
+++ src/Jhin.cpp
@@ -61,7 +61,7 @@ void Jhin::linerider()
     std::cout << "Press s to stop" << std::endl;
 
     BP.set_motor_dps(PORT_B, 1000);
-    BP.set_motor_dps(PORT_C, 500000);
+    BP.set_motor_dps(PORT_C, 50000);
 
     while (true)
     {
