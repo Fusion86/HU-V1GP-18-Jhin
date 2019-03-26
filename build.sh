9dd62d4cc677bf6c8d3b2347eacc1beafa192b66
Fusion86 <fusion86@cerbus.nl>
2019-03-26 16:20:58 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -2,5 +2,14 @@
 
 mkdir -p build
 cd build
-cmake ../src
-make
+
+if grep "Raspberry" /proc/device-tree/model; then
+    # Compile on Raspberry Pi
+    cmake -D CMAKE_BUILD_TYPE=Debug ../src
+    make
+else
+    # Cross-compile on linux for Raspberry Pi
+    # Requires that you have https://github.com/raspberrypi/tools in $HOME/rpi (see Toolchain-BrickPi.cmake)
+    cmake -D CMAKE_BUILD_TYPE=Debug -D CMAKE_TOOLCHAIN_FILE=../Toolchain-BrickPi.cmake ../src
+    make
+fi
