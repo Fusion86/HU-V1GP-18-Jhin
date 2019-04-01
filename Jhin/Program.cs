e75fff510f2100138a4068f145fbfc1cd3bf3ffe
Wouter Visser <fusion86@cerbus.nl>
2019-04-01 17:35:21 +0200

    Initial commit

---
+++ Jhin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jhin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
